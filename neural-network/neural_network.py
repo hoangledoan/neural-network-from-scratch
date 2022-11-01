4dccd198bb0901312919c59e5518099cf24b642f
Hoang Le Doan <ge59qat@mytum.de>
2022-11-01 15:15:40 +0100

    Add activation functions

--- neural-network/neural_network.py
+++ neural-network/neural_network.py
@@ -1,22 +1,45 @@
-from cmath import exp
-from xml.dom import xmlbuilder
 import numpy as np
-
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons)
-
-def first_layer(input_data, weight1, biases):
-    return np.dot(input_data, np.array(weight1).T) + biases
+import nnfs
+from nnfs.datasets import spiral_data
 
 
-def second_layer(output_first_layer, weight2, biases):
-    return np.dot(output_first_layer, np.array(weight2).T) + biases
+class Dense():
+
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        self.output = None
+
+    def forward(self, inputs_tensor) -> None:
+        self.output = np.dot(inputs_tensor, self.weights) + self.biases
 
 
-def sigmoid_activation(x):
-    """Return a value from 0 to 1."""
-    return 1/(1+exp(x))
+class Activation_Function():
 
-def relu_activation(x):
-    """Return x if x > 0, else 0."""
-    return max(0, x)
+    def __init__(self) -> None:
+        self.output = None
+
+    def sigmoid_activation(self, input_tensor):
+        """Return a value from 0 to 1."""
+        self.output = 1 / (1 + np.exp(input_tensor))
+
+    def relu_activation(self, input_tensor):
+        """Return x if x > 0, else 0."""
+        self.output = np.maximum(0, input_tensor)
+
+    def softmax_activation(self, input_tensor):
+        """Return a list of number which add up to 1."""
+        sum_list = [np.exp(number) for number in input_tensor]
+        e_sum = sum(sum_list)
+        self.output = [np.exp(number) / e_sum for number in input_tensor]
+
+
+if __name__ == "__main__":
+    X, y = spiral_data(samples=100, classes=3)
+    dense1 = Dense(2, 3)
+    dense1.forward(X)
+    dense2 = Dense(3, 6)
+    dense2.forward(dense1.output)
+    softmax = Activation_Function()
+    softmax.softmax_activation(dense2.output)
+    print(softmax.output)
